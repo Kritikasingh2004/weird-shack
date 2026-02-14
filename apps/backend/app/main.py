cbee1b7f183bcdf1fd69cef3be94d60524650ed2
Kritika Singh <kritikasingh00.com@gmail.com>
2026-02-14 22:22:13 +0530

    init

---
+++ apps/backend/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def app_def():
+    return{"Status":"Hello world"}
